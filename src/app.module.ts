9e620877b0567c20df71de415a800f25be1e4ef4
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-09-22 22:10:25 +0200

    cleanup nest boostrap module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GetGithubUserReposModule } from './get-github-user-repos';
 
 @Module({
   imports: [HttpModule, GetGithubUserReposModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
