cd897a94b525613cd6ef32bd40534297b32f6a7d
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-09-22 22:09:37 +0200

    add index.ts for export

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GetGithubUserReposModule } from './get-github-user-repos/get-github-user-repos.module';
+import { GetGithubUserReposModule } from './get-github-user-repos';
 
 @Module({
   imports: [HttpModule, GetGithubUserReposModule],
